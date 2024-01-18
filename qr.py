6741c38e01598855f9a283fbf5d5520b99c785fe
Alejandro Gonzalez Venegas <alex@MacBook-Air-de-Alejandro.local>
2024-01-18 20:55:43 +0100

    v1.1 - evita entrada nula y nombra fichero con datetime

--- qr.py
+++ qr.py
@@ -1,16 +1,27 @@
 import qrcode
-#from PIL import Image
+import datetime
+from PIL import Image
 
-# The data you want to encode in the QR code
-data = "https://github.com/Metalex84"
+# Fetching data from the user, avoiding empty input
+while True:
+    data = input("Please enter an URL: ")
+    if len(data) != 0:
+        break
 
-# Generate the QR code
+# Generating the QR code
 qr = qrcode.QRCode(version=1, box_size=7, border=1)
 qr.add_data(data)
 qr.make(fit=True)
 
-# Create an image from the QR code
+# Creating the image itself
 image = qr.make_image(fill="black", back_color="white")
 
-# Save the image
-image.save("myqrcode.png")
+# Building the filename
+current_timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = current_timestamp + ".png"
+
+# Saving the image
+image.save(filename)
+
+# Display the image
+Image.open(filename).show()
