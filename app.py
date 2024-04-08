e4209019a4bf6a28e5904953e78c22c2d1ea5498
AGV <agonzalez.venegas@outlook.com>
2024-04-08 17:59:15 +0200

    Llamada a 'os' para filesystem

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template, request
-import qrcode
-import datetime
 from PIL import Image
 from datetime import datetime
+from werkzeug.utils import secure_filename
+
+import qrcode
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/qrcodes'
 
 current_year = datetime.now().year
+
+
+    # TODO: paginas de error / renderizado errores
+    # TODO: Guardar la imagen igual en Linux, Mac y Windows
+
+
+
+def save_image(image, filename):
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html', current_year=current_year)
+
 
 
 @app.route('/generate', methods=['POST', 'GET'])
@@ -31,11 +48,12 @@ def generate():
         current_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         source = current_timestamp + ".png"
 
-        path = 'static/qrcodes/' + source
-        # TODO: Configurar guardado de imagen para que sea igual en Linux, Windows y Mac
-        # image.save(path)
+        # Guardo la imagen
+        # path = 'static/qrcodes/' + source
+        save_image(image, source)
+        
 
-        # TODO: Cuando eso esté listo, podré mostrar la imagen
+        # Muestro la imagen (puede que no sea necesario)
         # Image.open(path).show()
 
         path = 'qrcodes/' + source
